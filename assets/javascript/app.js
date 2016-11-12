f08e22016311280d76969dcef7ae1044a867764f
Our Laptop <Our Laptop>
2016-11-12 13:28:01 -0800

    fixed merg conflicts

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,32 +1,22 @@
 $(document).ready(function() {
-<<<<<<< HEAD
-    // Initialize Firebase
-// Test function for Justin to see
-});
-=======
 
 });
 
 // Get the modal
 var modal = document.getElementById('myModal');
 
-// Get the button that opens the modal
 var btn = document.getElementById("log-in");
 
-// Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks the button, open the modal
 btn.onclick = function() {
     modal.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
 span.onclick = function() {
     modal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
@@ -34,4 +24,3 @@ window.onclick = function(event) {
 }
 
 
->>>>>>> 726884d9401964c45be5d42803b3a1872125f2af
